d74d7b6980659b3b26d1dca85987d6c5912eec90
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-07-31 15:56:32 -0300

    Ho to create a simple calculate in ruby.

---
+++ Calculate.rb
@@ -0,0 +1,7 @@
+# Calculadora de soma!
+puts "input a number"
+n1 = gets.chomp.to_i
+puts "input a other number"
+n2 = gets.chomp.to_i
+soma = n1 + n2 
+puts "A soma entre #{n1} + #{n2} é igual a #{soma}"
